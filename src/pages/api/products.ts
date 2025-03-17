ebe42a2802d12bdc2ea0a531a237457094f48008
Andres Tobio <92232669+AndrewUru@users.noreply.github.com>
2025-03-18 00:46:14 +0100

    Feat: Implementar API interna para cargar productos desde un archivo JSON y mejorar la visualización en la página de tienda

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -1,30 +1,14 @@
+import fs from "fs/promises";
+import path from "path";
+
 export async function GET() {
-  const url = "https://www.alvafusiondesigns.com/wp-json/wc/v3/products";
-  const consumerKey = "ck_2ef54305a84705add80d08544d8d39383283d485";
-  const consumerSecret = "cs_8f282e44597098c5b27d20726252cfa942d39017";
-
-  // Autenticación básica (Base64)
-  const auth = Buffer.from(`${consumerKey}:${consumerSecret}`).toString(
-    "base64",
-  );
-
   try {
-    const res = await fetch(url, {
-      headers: {
-        Authorization: `Basic ${auth}`,
-        "Content-Type": "application/json",
-      },
+    const filePath = path.resolve("./src/data/products.json");
+    const data = await fs.readFile(filePath, "utf-8");
+    return new Response(data, {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
     });
-
-    if (!res.ok) {
-      return new Response(
-        JSON.stringify({ error: `Error ${res.status}: ${res.statusText}` }),
-        { status: res.status },
-      );
-    }
-
-    const data = await res.json();
-    return new Response(JSON.stringify(data), { status: 200 });
   } catch (error) {
     return new Response(
       JSON.stringify({ error: "Error al obtener los productos" }),
